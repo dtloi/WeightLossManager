0b16d48162129c4b382b905a04abb2f4c0e3d7d3
Andrew Stenger <43499073+andrewStenger@users.noreply.github.com>
2020-06-22 18:02:08 -0400

    added function to calculate lbs lost over period of days

--- WeightCalculations.py
+++ WeightCalculations.py
@@ -1,3 +1,16 @@
+from datetime import datetime
+# a collection of helper methods for WeightLoss Manager that perform calculations involving weight
+
+
+# get the estimated number of lbs lost between today and the given date with the given number of lbs lost per week
+def lbsLost(date, lbsLoss, dateFormat="%m/%d/%Y"):
+	now = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
+	later = datetime.strptime(date, dateFormat)
+	days = (later - now).days
+
+	# x lbs per week is equal to x/7 lbs per day
+	return days*(lbsLoss/7)
+
 
 
 # calculate the user's Basal Metabolic Rate
@@ -7,27 +20,30 @@ def BMR(sex, age, weight, height, units=('lbs', 'inches')):
 	# if weight in pounds, convert to kg
 	if units[0] == 'lbs':
 		weight *= 0.453592
+
 	elif units[0] != 'kg':
 		print("Error: weight must be in either lbs or kg");  exit()
 
 	# convert inches to centimeters
 	if units[1] == 'inches':
 		height *= 2.54
+
 	elif units[1] != 'cm':
 		print("Error: height must be in either inches or cm");  exit()
 
 	main_calculation = 10*weight + 6.25*height - 5*age
 
 	# the sex of the user creates either a bonus or decrease to the permitted-calorie calculation
-	if sex.upper() == 'M':
+	sexID = sex.upper()
+	if sexID == "M" or sexID == "MALE":
 		return main_calculation + 5
 
-	elif sex.upper() == 'F':
+	elif sexID == "F" or sexID == "FEMALE":
 		return main_calculation - 161
 
 	else:
-		print('Error: sex must be either Male or Female')
-		exit()
+		print('Error: sex must be either Male or Female');  exit()
+
 
 
 # return the number of calories the user should consume to remain at their current weight
@@ -39,11 +55,14 @@ def CaloricNeeds(sex, age, weight, height, activityLevel, units=('lbs', 'inches'
 	return activityLevelMultipliers[activityLevel]*BMR(sex, age, weight, height, units)
 
 
+
+# testing
 if __name__ == '__main__':
-	sex = 'Female'
-	age = 20
-	weight = 56.425
-	height = 152.4
+	sex = 'Male'
+	age = 26
+	weight = 240
+	height = 73
 	activityLevel = 'sedentary'
 	print(BMR(sex, age, weight, height))
 	print(CaloricNeeds(sex, age, weight, height, activityLevel))
+	print(lbsLost("07/6/2020", 2))
