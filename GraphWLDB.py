643d8bff81b0343abd319de5c09f751e7c3f5aa2
Andrew Stenger <43499073+andrewStenger@users.noreply.github.com>
2020-06-25 13:08:06 -0400

    added ability to graph weightloss plan vs. user weightloss

--- GraphWLDB.py
+++ GraphWLDB.py
@@ -1,40 +1,47 @@
 import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
-import pandas.io.sql as psql
-from datetime import datetime
-from numpy import fromiter, datetime64, timedelta64
+from datetime import timedelta
+from datetime import datetime as dt
+from pandas import DataFrame
+from decimal import Decimal
 
 
 
-def byMonth(data, name, datemin=None, datemax=None):
+# graph the user's weight data over time compared to their projected weight-loss
+# using a "lost lbs. per week" plan
+def GraphByPlan(dateDF, name, weeklyLoss):
 
-	if datemin is None:
-		datemin = data['date'][0]
-	if datemax is None:
-		datemax = data['date'][len(data)-1]
-	
-	years = mdates.YearLocator()   # every year
-	months = mdates.MonthLocator()  # every month
-	days = mdates.DayLocator()
-	monthsFormat = mdates.DateFormatter('%B')
 	fig, ax  =  plt.subplots(figsize=(11,9))
-	# must account for if over a year, then months code will not work
 
-	ax.plot('date', 'weight', data=data)
+	# construct dataframe for weight-loss plan for comparison
+	wlpDF = dateDF.copy()
+	firstWeight = dateDF['weight'].values[0]
+	firstDate = dt.strptime(dateDF['date'].values[0], "%Y-%m-%d").date()
+
+	# iterate through copy and change weights to projected 
+	for index, row in wlpDF.iterrows():
+		thisDate = dt.strptime(row['date'], "%Y-%m-%d").date()
+		dateDifference = (thisDate - firstDate).days
+		wlpDF.at[index, 'weight'] = firstWeight - (weeklyLoss/7)*dateDifference
+
+	# plot user's weight
+	ax.plot('date', 'weight', data=dateDF, label="Actual Weight", marker='o', color='red')
+	ax.plot('date', 'weight', data=dateDF, label="_nolegend_", color='red')
+
+	# plot projected weight
+	ax.plot('date', 'weight', data=wlpDF, label="Projected (at %s lbs. per week)"
+			% Decimal(round(weeklyLoss,1)), marker='o', color='blue')
+	ax.plot('date', 'weight', data=wlpDF, label="_nolegend_", color='blue')
+
+	# adjust plot settings
 	ax.set_title("%s's Weight  (in lbs.)" % name, fontsize=18)
-
-	# round to nearest months
-
-	ax.set_xlim(datemin, datemax)
-
-	# format the coords message box
-	ax.format_xdata = mdates.DateFormatter("%m/%d/%Y")
-	ax.format_ydata = lambda x: '$%1.2f' % x
-	ax.grid(False)
+	ax.legend(loc="upper right")
+	ax.set_xlabel("Date", fontsize=15)
+	ax.set_ylabel("Weight", fontsize=15)
+	ax.grid(True)
 
 	# rotates and right aligns the x labels, and moves the bottom of the
 	# axes up to make room for them
 	fig.autofmt_xdate()
 
 	plt.show()
-
